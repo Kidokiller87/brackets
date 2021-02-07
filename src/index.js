5f62a24c804b6510292840714977f4908cfc65d6
Kidokiller87 <kidokiller87@gmail.com>
2021-02-07 17:50:40 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  bracketsConfig.map(e=> stack.push(e.join('')))
+
+  for (let i = 0; i < stack.length; i++) {
+    if (str.includes(stack[i])) {
+        str = str.replace(stack[i], '');
+        i = -1;
+    }
 }
+return str.length !== 0 ? false : true;
+};
+
